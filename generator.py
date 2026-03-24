b5c4e75b05014b873cb802750b89b0f60448ac0b
Etienne Dreyer <etienne.dreyer@weizmann.ac.il>
2026-03-24 12:09:03 +0200

    separate out generator

---
+++ generator.py
@@ -0,0 +1,33 @@
+import torch
+
+class EventGenerator:
+
+    def __init__(self, config):
+        self.x_min, self.x_max = config['x_range']
+        self.y_min, self.y_max = config['y_range']
+        self.E_min, self.E_max = config['E_range']
+        self.N_min, self.N_max = config['N_range']
+
+    def generate(self, N_events=1):
+
+        N_particles = torch.randint(self.N_min, self.N_max + 1, (N_events,)).tolist()
+
+        N_pad = max(N_particles)
+
+        ### Uniform spread in x/y
+        particle_xs = torch.rand((N_events, N_pad)) * (self.x_max - self.x_min) + self.x_min
+        particle_ys = torch.rand((N_events, N_pad)) * (self.y_max - self.y_min) + self.y_min
+
+        ### Power-law spectrum in energy
+        alpha = 2.0
+        r = torch.rand((N_events, N_pad))
+        particle_Es = ((self.E_max**(1-alpha) - self.E_min**(1-alpha)) * r + self.E_min**(1-alpha))**(1/(1-alpha))
+
+        if N_events == 1:
+            return particle_Es[0], particle_xs[0], particle_ys[0]
+
+        ### Mask out padded particles, vectorized
+        for i, N in enumerate(N_particles):
+            particle_Es[i, N:] = 0.0  # zero energy means "no particle"
+
+        return particle_Es, particle_xs, particle_ys
